2c863598433773cbfcfabe785ba80bfe422371ff
BlackCNP <mrx30050@gmail.com>
2023-12-25 15:21:24 +0200

    Game

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -7,12 +7,13 @@ public class PlayerLife : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
-
+    [SerializeField] private AudioSource Diesound;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
+   
 
     private  void OnCollisionEnter2D(Collision2D collision)
     {
@@ -21,11 +22,14 @@ public class PlayerLife : MonoBehaviour
             Die();
         }
     }
+  
 
     private void Die()
     {
+        Diesound.Play();
         rb.bodyType = RigidbodyType2D.Static;
         anim.SetTrigger("death");
+      
     }
 
     private void RestartLevel()
